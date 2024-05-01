68493b9fb251974c1adce91af839e99f64625ae3
Aleksey Bogdanov <aleksey@bogdanov77.ru>
2024-05-01 09:31:28 +0300

    add data.py, models.py, sqlite3.db

--- data/data.py
+++ data/data.py
@@ -1,7 +1,20 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from models import create_tables, User, EngWord, RusWord
 
 engine = create_engine('sqlite:///sqlite3.db')
 engine.connect()
+
+
+create_tables(engine)
+
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+session.close()
 
 if __name__ == '__main__':
     print(engine)
--- data/models.py
+++ data/models.py
@@ -1,16 +1,46 @@
-import sqlalchemy as sq
 from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 Base = declarative_base()
 
 
+class UserToEngWord(Base):
+    __tablename__ = 'user_eng_word'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    eng_word_id = Column(Integer, ForeignKey('eng_word.id'))
+
+
 class User(Base):
-    pass
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    user_tg_id = Column(Integer, nullable=False, unique=True)
+    first_name = Column(String(length=50))
+    last_name = Column(String(length=50))
+    username = Column(String(length=50))
+    rus_words = relationship('UserToEngWord', backref='user')
 
 
 class EngWord(Base):
-    pass
+    __tablename__ = 'eng_word'
+
+    id = Column(Integer, primary_key=True)
+    eng_word = Column(String)
+    rus_word_id = Column(Integer, ForeignKey('rus_word.id'))
+
+    rus_word = relationship('RusWord', backref='eng_words')
+    user = relationship('UserToEngWord', backref='eng_words')
 
 
 class RusWord(Base):
-    pass
+    __tablename__ = 'rus_word'
+
+    id = Column(Integer, primary_key=True)
+    rus_word = Column(String)
+
+
+def create_tables(engine):
+    # Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
