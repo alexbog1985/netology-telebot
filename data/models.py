85abd510113d0305e7d2781a2fc883e79771d22e
Aleksey Bogdanov <aleksey@bogdanov77.ru>
2024-05-18 20:32:37 +0300

    change DB structure

--- data/models.py
+++ data/models.py
@@ -4,44 +4,34 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 Base = declarative_base()
 
 
-class UserToEngWord(Base):
-    __tablename__ = 'user_eng_word'
+class UserWord(Base):
+    __tablename__ = 'user_word'
 
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('user.id'))
-    eng_word_id = Column(Integer, ForeignKey('eng_word.id'))
+    word_id = Column(Integer, ForeignKey('word.id'))
 
 
 class User(Base):
     __tablename__ = 'user'
 
-    id = Column(Integer, primary_key=True)
-    user_tg_id = Column(Integer, nullable=False, unique=True)
-    user_chat_id = Column(Integer, nullable=False, unique=True)
+    id = Column(Integer, primary_key=True, autoincrement=False)
     first_name = Column(String(length=50))
     last_name = Column(String(length=50))
     username = Column(String(length=50))
     step = Column(Integer, default=0)
 
-    eng_words = relationship('UserToEngWord', backref='users')
+    words = relationship('UserWord', backref='users')
 
 
-class EngWord(Base):
+class Word(Base):
     __tablename__ = 'eng_word'
 
     id = Column(Integer, primary_key=True)
-    eng_word = Column(String)
-    rus_word_id = Column(Integer, ForeignKey('rus_word.id'))
+    eng = Column(String)
+    rus = Column(String)
 
-    rus_word = relationship('RusWord', backref='rus_words')
-    users = relationship('UserToEngWord', backref='eng_words')
-
-
-class RusWord(Base):
-    __tablename__ = 'rus_word'
-
-    id = Column(Integer, primary_key=True)
-    rus_word = Column(String)
+    users = relationship('UserWord', backref='words')
 
 
 def create_tables(engine):
